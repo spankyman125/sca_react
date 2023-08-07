f00bc967244fa4e8d88a10f762ccff2de3365e32
spankyman125 <spankyman@live.ru>
2023-08-07 13:22:42 +0700

    Remove console.logs

--- src/stores/AuthStore.ts
+++ src/stores/AuthStore.ts
@@ -1,9 +1,9 @@
-import AuthAPI from '../api/http/Auth';
-import UsersAPI from '../api/http/Users';
-import { User } from '../api/http/interfaces';
 import Cookies from 'js-cookie';
 import jwt_decode from 'jwt-decode';
 import { makeAutoObservable, runInAction } from 'mobx';
+import AuthAPI from '../api/http/Auth';
+import UsersAPI from '../api/http/Users';
+import { User } from '../api/http/interfaces';
 
 export enum AuthStatus {
   Authorized = 'authorized',
@@ -35,8 +35,6 @@ class AuthStore {
 
   async restoreSession() {
     const rememberedRefreshToken = Cookies.get('refresh_token');
-    console.log('Cookies init, refresh_token: ', Cookies.get('refresh_token'));
-    console.log('Cookies init, document cookie: ', document.cookie);
     if (rememberedRefreshToken) {
       try {
         const { access_token, refresh_token } = await AuthAPI.refresh(
@@ -68,37 +66,26 @@ class AuthStore {
   }
 
   async login(username: string, password: string, remember = false) {
-    console.log('login');
     try {
       const { access_token, refresh_token } = await AuthAPI.login(
         username,
         password,
       );
       runInAction(() => {
-        console.log('Setting auth tokens');
         this.access_token = access_token;
         this.refresh_token = refresh_token;
         this.remember = remember;
       });
-      console.log('Getting user info from api');
       const user = await UsersAPI.me();
       const friends = await UsersAPI.getFriendsSelf();
       runInAction(() => {
-        console.log('Setting user info from api');
         this.user = { ...user, friends };
         this.status = AuthStatus.Authorized;
       });
-      console.log('Setting refresh cookie');
       if (remember) Cookies.set('refresh_token', refresh_token);
-      console.log(
-        'Cookies after set, refresh token: ',
-        Cookies.get('refresh_token'),
-      );
-      console.log('Cookies after set, document cookie: ', document.cookie);
 
       // SocketService.init(access_token);
     } catch (e) {
-      console.log('Error occured', e);
       runInAction(() => {
         this.status = AuthStatus.Unauthorized;
       });
