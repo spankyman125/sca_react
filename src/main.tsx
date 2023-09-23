d5f19ca91221925e1a30f76ca3b73a5a32c3bb13
spankyman125 <spankyman@live.ru>
2023-09-23 14:34:39 +0600

    Fix ts build errors

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,6 @@ import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import { CssBaseline, ThemeProvider, createTheme } from '@mui/material';
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {
   Navigate,
